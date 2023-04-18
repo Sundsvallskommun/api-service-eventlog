ab7272d45960d140badc974d122f98f458e86f0a
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2023-04-18 14:01:51 +0200

    UF-4931: Add API resource

---
+++ src/main/java/se/sundsvall/eventlog/api/model/EventType.java
@@ -0,0 +1,26 @@
+package se.sundsvall.eventlog.api.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(enumAsRef = true, description = """
+## CRUD events:
+* `CREATE`
+* `READ`
+* `UPDATE`
+* `DELETE`
+## System events:
+* `ACCESS` - login, logout, authorization, throttling
+* `EXECUTE` - send, queue, schedule, transfer, move
+* `CANCEL` - terminate, abort, decommission, kill
+* `DROP` - skip, omit, ignore, disregard
+""")
+public enum EventType {
+	CREATE,
+	READ,
+	UPDATE,
+	DELETE,
+	ACCESS,
+	EXECUTE,
+	CANCEL,
+	DROP
+}
---
+++ src/test/java/se/sundsvall/eventlog/api/model/EventTypeTest.java
@@ -0,0 +1,21 @@
+package se.sundsvall.eventlog.api.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static se.sundsvall.eventlog.api.model.EventType.ACCESS;
+import static se.sundsvall.eventlog.api.model.EventType.CANCEL;
+import static se.sundsvall.eventlog.api.model.EventType.CREATE;
+import static se.sundsvall.eventlog.api.model.EventType.DELETE;
+import static se.sundsvall.eventlog.api.model.EventType.DROP;
+import static se.sundsvall.eventlog.api.model.EventType.EXECUTE;
+import static se.sundsvall.eventlog.api.model.EventType.READ;
+import static se.sundsvall.eventlog.api.model.EventType.UPDATE;
+
+class EventTypeTest {
+
+	@Test
+	void enums() {
+		assertThat(EventType.values()).containsExactly(CREATE, READ, UPDATE, DELETE, ACCESS, EXECUTE, CANCEL, DROP);
+	}
+}
